1564b93c90801d2e505540f7e5939fb4937e5734
Casey C. Ross <caseycross718@gmail.com>
2017-02-03 14:41:26 -0500

    class notes created and updated, lab 1 part 1 complete
    class notes created and updated, lab 1 part 1 complete

---
+++ class_notes.js
@@ -0,0 +1,28 @@
+// custom markers for leaflet --> uses an options object
+      // this is the basic "insert marker" format for leaflet:
+          L.marker([lat, long]).addTo(map);
+
+      // marker options allow you to configure the marker and are passed through an object
+          L.marker([lat, long],{options:boolean}).addTo(map); // This is the basic format for options
+          L.marker([lat, long],{draggable:true}).addTo(map); // This is a specific example of how to use those options
+
+
+      //alternately, you could define the new object
+
+          var optionsMine = {
+            draggable: true,
+            opacity: 0.5,
+          };
+
+          L.marker([lat, long],{optionsMine}).addTo(map); // in this case you set all the options before hand in the variable and then call them
+
+      // Another way to look at / think about Options objects:
+
+          var muOptions = {
+            "name" : "Casey",
+            "number": 123123,
+          };
+
+          var generateID = function (options) {
+            return options.name + options.number;
+          };
--- lab/lab1/part1.js
+++ lab/lab1/part1.js
@@ -63,7 +63,7 @@ Is printMenu a function? Answer this question with underscore. Should evaluate
 to true.
 ===================== */
 
-var query1;
+var query1 = _.isFunction(printMenu);
 
 console.log('printMenu is a function:', query1);
 
@@ -72,7 +72,7 @@ Is bakedGoods an array? Answer this question with underscore. Should evaluate
 to true.
 ===================== */
 
-var query2;
+var query2 = _.isArray(bakedGoods);
 
 console.log('bakedGoods is an array:', query2);
 
@@ -81,7 +81,7 @@ Is the first element in bakedGoods an object? Answer this question with
 underscore. Should evaluate to true.
 ===================== */
 
-var query3;
+var query3 = _.isObject(bakedGoods[0]);
 
 console.log('The first element in bakedGoods is an object:', query3);
 
@@ -89,31 +89,41 @@ console.log('The first element in bakedGoods is an object:', query3);
 Use _.where to return all cakes. Or bread. Whichever is your favorite.
 ===================== */
 
-var query4;
+var query4= _.where(bakedGoods, {"type": "Cake"});
 
 console.log('All bread. Or cakes:', query4);
 
 /* =====================
 Use _.filter to return all baked goods that cost more than $4.
 ===================== */
+    // _.filter(list, predicate, [context])
+    // var evens = _.filter([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
+    // => [2, 4, 6]
 
-var query5;
+var query5 = _.filter(bakedGoods, function(object){ return object.price > 4; });
 
 console.log('More than $4:', query5);
 
 /* =====================
 Use _.sortBy to order the list by inventory (from lowest to highest).
 ===================== */
+    // _.sortBy(list, iteratee, [context])
+    // var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
+    // _.sortBy(stooges, 'name');
+    // => [{name: 'curly', age: 60}, {name: 'larry', age: 50}, {name: 'moe', age: 40}];
 
-var query6;
+var query6 = _.sortBy(bakedGoods, "inventory");
 
 console.log('Sorted by inventory (lowest to highest):', query6);
 
 /* =====================
 Use _.groupBy to organize the baked goods by type.
 ===================== */
+    // _.groupBy(list, iteratee, [context])
+    // _.groupBy(['one', 'two', 'three'], 'length');
+    // => {3: ["one", "two"], 5: ["three"]}
 
-var query7;
+var query7 = _.groupBy(bakedGoods, "type");
 
 console.log('Grouped by type:', query7);
 
